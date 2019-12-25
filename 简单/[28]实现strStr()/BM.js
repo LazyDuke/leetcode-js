53c5a717c9a96a77d2d39c4f43b0e255ad885f75
懒成铁 <weiguocai.fzu@gmail.com>
2019-12-25 21:21:15 +0800

    feat(*):add BM and Sunday algorithm

--- 简单/[28]实现strStr()/BM.js
+++ 简单/[28]实现strStr()/BM.js
@@ -12,17 +12,20 @@
  */
 var strStr = function(haystack, needle) {
   const SIZE = 256
+  const m = needle.length
+  const n = haystack.length
 
-  if (needle.length === 0) {
+  if (m === 0) {
     return 0
   }
 
-  const bmBc = preBmBc(needle)
+  const bmBc = generateBmBc(needle)
+  const { prefix, suffix } = generateBmGs(needle)
 
   let i = 0
-  while (i <= haystack.length - needle.length) {
+  while (i <= n - m) {
     let j
-    for (j = needle.length - 1; j >= 0; --j) {
+    for (j = m - 1; j >= 0; --j) {
       if (haystack[i + j] !== needle[j]) {
         break
       }
@@ -32,7 +35,13 @@ var strStr = function(haystack, needle) {
       return i
     }
 
-    i = i + (j - bmBc[haystack.charCodeAt(i + j)])
+    let x = j - bmBc[haystack.charCodeAt(i + j)]
+    let y = 0
+    if (j < m - 1) {
+      y = moveByBmGs(j, m, suffix, prefix)
+    }
+
+    i = i + Math.max(x, y)
   }
 
   return -1
@@ -40,13 +49,69 @@ var strStr = function(haystack, needle) {
   /**
    * @param {string} needle
    * */
-  function preBmBc(needle) {
+  function generateBmBc(needle) {
+    const m = needle.length
     const bmBc = new Array(SIZE).fill(-1)
-    for (let i = 0; i < needle.length; i++) {
+    for (let i = 0; i < m; ++i) {
       const ascii = needle.charCodeAt(i)
       bmBc[ascii] = i
     }
     return bmBc
+  }
+
+  /**
+   * @param {string} needle
+   */
+  function generateBmGs(needle) {
+    const m = needle.length
+
+    const suffix = []
+    const prefix = []
+
+    for (let i = 0; i < m; ++i) {
+      suffix[i] = -1
+      prefix[i] = false
+    }
+
+    for (let i = 0; i < m - 1; ++i) {
+      let j = i
+      let k = 0
+      while (j >= 0 && needle[j] === needle[m - 1 - k]) {
+        --j
+        ++k
+        suffix[k] = j + 1
+      }
+
+      if (j === -1) {
+        prefix[k] = true
+      }
+    }
+
+    return {
+      prefix,
+      suffix
+    }
+  }
+
+  /**
+   * @param {number} i
+   * @param {number} m
+   * @param {number[]} suffix
+   * @param {boolean[]} prefix
+   */
+  function moveByBmGs(j, m, suffix, prefix) {
+    let k = m - 1 - j
+    if (suffix[k] !== -1) {
+      return j - suffix[k] + 1
+    }
+
+    for (let r = j + 2; r <= m - 1; ++r) {
+      if (prefix[m - r]) {
+        return r
+      }
+    }
+
+    return m
   }
 }
 // @lc code=end
--- 简单/[28]实现strStr()/Sunday.js
+++ 简单/[28]实现strStr()/Sunday.js
@@ -14,5 +14,38 @@ var strStr = function(haystack, needle) {
   if (needle.length === 0) {
     return 0
   }
+
+  const slen = haystack.length
+  const plen = needle.length
+
+  let i = 0
+  let j = 0
+  let m = plen
+  let k
+
+  while (i < slen) {
+    if (haystack[i] !== needle[j]) {
+      for (k = plen - 1; k >= 0; k--) {
+        if (needle[k] === haystack[m]) {
+          break
+        }
+      }
+
+      i = m - k
+      j = 0
+      m = i + plen
+      if (m > slen) {
+        return -1
+      }
+    } else {
+      if (j === plen - 1) {
+        return i - j
+      }
+      i++
+      j++
+    }
+  }
+
+  return -1
 }
 // @lc code=end
