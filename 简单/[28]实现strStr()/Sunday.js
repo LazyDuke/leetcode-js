9b8cd8a9e0c57a2c8c8e63afe00b9dbba1e710c7
懒成铁 <weiguocai.fzu@gmail.com>
2020-01-24 16:28:10 +0800

    docs(Sunday):add some comments

--- 简单/[28]实现strStr()/Sunday.js
+++ 简单/[28]实现strStr()/Sunday.js
@@ -18,29 +18,35 @@ var strStr = function(haystack, needle) {
   const slen = haystack.length
   const plen = needle.length
 
-  let i = 0
-  let j = 0
-  let m = plen
-  let k
+  let i = 0 // 主串位置指针
+  let j = 0 // 模式串位置指针
+  let m = plen // 每次重新匹配时，模式串尾部对应主串位置的下一位
+  let k // 失配时模式串回溯位置指针
 
   while (i < slen) {
+    // 如果失配
     if (haystack[i] !== needle[j]) {
+      // 遍历模式串，查看模式串中是否包含m位置的字符
       for (k = plen - 1; k >= 0; k--) {
         if (needle[k] === haystack[m]) {
           break
         }
       }
-
+      // 确定包含字符的位置，即确定模式串移动位置，重新开始匹配
       i = m - k
       j = 0
       m = i + plen
+      // 如果m超出主串长度，表示模式串已经无法匹配
       if (m > slen) {
         return -1
       }
+      // 如果匹配
     } else {
+      // 如果j已经遍历一次模式串，表示模式串匹配完成
       if (j === plen - 1) {
         return i - j
       }
+      // 未匹配完成则继续匹配
       i++
       j++
     }
